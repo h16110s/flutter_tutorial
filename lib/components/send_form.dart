2426c3f41dc2327d34e77589162d8704fda4baf5
Hirohito <h_16110s@yahoo.co.jp>
2023-06-15 16:00:09 +0900

    post message

--- lib/components/send_form.dart
+++ lib/components/send_form.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_tutorial/app_state.dart';
 import 'package:flutter_tutorial/chat_data.dart';
@@ -14,6 +17,33 @@ class SendForm extends StatefulWidget {
 
 class _SendFormState extends State<SendForm> {
   final _editController = TextEditingController();
+
+  Future<Map> sendChatMessage(String message) async {
+    await dotenv.load(fileName: '.env');
+    String apiKey = dotenv.get('API_KEY');
+
+    var response = await http.post(
+      // TODO: constとして定義しておくといい
+      Uri.parse('https://api.openai.com/v1/chat/completions'),
+      headers: {
+        'Authorization': 'Bearer $apiKey',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        "model": "gpt-3.5-turbo-0613",
+        "max_tokens": 256,
+        "messages": [
+          {
+            "role": "system",
+            "content":
+                "Please make all responses in sentences of up to 256 tokens."
+          },
+          {"role": "user", "content": message},
+        ]
+      }),
+    );
+    return jsonDecode(utf8.decode(response.bodyBytes));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,9 +67,12 @@ class _SendFormState extends State<SendForm> {
         ),
         IconButton(
           onPressed: () {
-            appState.addHistory(  
-              ChatData(ChatMessageType.sent,appState.current)
-            );
+            String current = appState.current;
+            appState.addHistory(ChatData(ChatMessageType.sent, current));
+            sendChatMessage(current).then((response) {
+              String msg = response['choices'][0]['message']['content'];
+              appState.addHistory(ChatData(ChatMessageType.received, msg));
+            });
             _editController.clear();
           },
           icon: const Icon(Icons.send),
