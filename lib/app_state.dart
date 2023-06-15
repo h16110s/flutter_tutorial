0ab7e2051c572cb74345c01a91cfc2e035bd120d
h16110s <hirohito-saito@infinityloop.co.jp>
2023-06-15 11:18:18 +0900

    clear text field

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_tutorial/chat_data.dart';
 
 class AppState extends ChangeNotifier {
+  String current = '';
   List<ChatData> history = <ChatData>[
     ChatData(ChatMessageType.received, 'こんにちは'),
   ];
 
   void addHistory(ChatData chatData) {
     history.insert(0, chatData);
+    notifyListeners();
+  }
+
+  void setCurrent(String text) {
+    current = text;
     notifyListeners();
   }
 
--- lib/components/send_form.dart
+++ lib/components/send_form.dart
@@ -1,26 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_tutorial/app_state.dart';
+import 'package:flutter_tutorial/chat_data.dart';
 
-class SendForm extends StatelessWidget {
+class SendForm extends StatefulWidget {
   const SendForm({
     super.key,
   });
 
   @override
+  State<SendForm> createState() => _SendFormState();
+}
+
+class _SendFormState extends State<SendForm> {
+  final _editController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    // appStateの取得
+    var appState = context.watch<AppState>();
+
     return Row(
       children: <Widget>[
-        const Flexible(
+        Flexible(
           child: TextField(
-            decoration: InputDecoration(
+            controller: _editController,
+            decoration: const InputDecoration(
               border: OutlineInputBorder(),
               hintText: 'Enter a search term',
             ),
+            onChanged: (String text) {
+              appState.setCurrent(text);
+            },
           ),
         ),
         IconButton(
           onPressed: () {
-            print('btn pressed');
+            appState.addHistory(  
+              ChatData(ChatMessageType.sent,appState.current)
+            );
+            _editController.clear();
           },
           icon: const Icon(Icons.send),
           color: theme.colorScheme.primary,
